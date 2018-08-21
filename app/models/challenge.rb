2b0e0afad927a58cd050978d61d98136cbec7712
Gabriel-Batista <gabedbatista@gmail.com>
2018-08-21 12:09:32 -0400

    add logic to challenge model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,22 @@
 class Challenge < ApplicationRecord
   has_many :user_challenges
   has_many :users, through: :user_challenges
+
+  def match_value(regex)
+    @test_string.scan(regex).captures
+  end
+
+  def total_matches
+      self.match_value(@test_string).length
+  end
+
+  def result
+      @solution == match_value
+  end
+
+  def to_s
+      puts "Challenge: #{challenge_name}"
+      puts "String: #{test_string}"
+      puts "Solution: #{solution}"
+  end
 end
