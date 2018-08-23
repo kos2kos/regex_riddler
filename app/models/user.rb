b442304b20f5d70462cd64f5558572dd075b657e
Gabriel-Batista <gabedbatista@gmail.com>
2018-08-23 18:52:56 -0400

    Fixed regex bugs which surfaced when creating seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,9 @@ class User < ApplicationRecord
   def url_edit
     "http://localhost:3000/user/#{self.id}/edit"
   end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
